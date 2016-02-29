78647c421be0c9dc7fd9739767da31c87577e35b
alidio <alidionysis@yahoo.gr>
2016-02-29 15:22:08 +0200

    29/02/2016 gerakas --->1

--- src/company/RestWorkPermit.java
+++ src/company/RestWorkPermit.java
@@ -2,7 +2,9 @@
 //αδειών από κάθε τύπο. 
 package company;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 import model.Availableworkpermit;
 import model.Employee;
 import model.Workpermittype;
@@ -14,26 +16,58 @@ public class RestWorkPermit {
     private Date maxDate; //Ημνία ΈΩΣ της τελευταίας άδειας
     private Date fromDate; //Η άδεια που θα καταχωρηθεί θα αρχίζει απο fromDate
     private Date toDate;  //Η άδεια που θα καταχωρηθεί θα αρχίζει απο toDate
+    private final Random rnd = new Random(); //Παραγωγή τυχαίων αριθμών
+    private int numdays; //Πόσες ημέρες άδεια θα καταχωρηθεί (τυχαίο νουμερο με max το ypoloipo)
 
     public RestWorkPermit(Employee emp, Availableworkpermit AworkPermit, int sumNdays, Date maxDate) {
         this.emp = emp;
         this.AworkPermit = AworkPermit;
-        this.maxDate = maxDate;
+        this.maxDate = fixmaxDate(maxDate);
         this.ypoloipo = AworkPermit.getAvailableDays() - sumNdays;
         
         //Υπολογισμός ημ/νίας έναρξης-λήξης της άδειας που θα καταχωρηθεί.        
         calcFromToDate();
-       
+      
     }
+    
+    //Εάν η ημερομηνία είναι null επιστρέφει 01/01/Τρέχοντος έτους
+    private Date fixmaxDate(Date maxDate){
+        
+        if (!(maxDate == null)) return maxDate;
+        
+        Date retDate;
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+
+        cal.set(year, 1, 1);
+        retDate = cal.getTime();
+        return retDate;   
+    }
+    
     
     //Η μέθοδος calcFromToDate()υπολογίζει τις ημερομηνίες απο-έως που 
     //χρειάζονται για να γίνει η καταχώρηση της νέας άδειας.
     private void calcFromToDate(){
-        //Από τις 365 ημέρες του χρόνου
-        int YearDays = 365;
         
-        //αφαιρώ όσες είναι η maxDate//να τη φτιαξω...
-
+        //From Date = max date + random number(x)
+        //To Date = From Date + random number(y)
+        //x->Τυχαίο νούμερο σε ημέρες μετά την τελευταία άδεια 
+        //και πρίν το τέλος του χρόνου
+        //y->Τυχαίο νούμερο σε ημέρες ανάλογα με το υπόλοιπο του 
+        //τύπου άδειας.
+        
+        Calendar cal = Calendar.getInstance(); 
+        cal.setTime(maxDate);
+        //Στην τελευταία άδεια, προσθέτω ένα αριθμό ημερών μεταξύ της τελευταίας άδειας
+        //και το τέλος του χρόνου
+        numdays = 1 + rnd.nextInt(365 - cal.get(Calendar.DAY_OF_YEAR) - ypoloipo);
+        cal.add(Calendar.DAY_OF_MONTH, numdays);
+        this.fromDate = cal.getTime();
+        
+        //Στην from date προσθέτω ένα τυχαίο άριθμό από το 1 έως το 
+        //υπόλοιπο άδειας που έχει για να προσθέσω την άδεια.
+        cal.add(Calendar.DAY_OF_MONTH, 1+rnd.nextInt(ypoloipo));
+        this.toDate = cal.getTime();
     }
     
     public Date getMaxDate() {
